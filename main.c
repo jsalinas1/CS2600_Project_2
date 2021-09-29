6a7f082ba9fbfe4b158b0d3cc089cc333de658cd
Jezreel Salinas <jsalinas@cpp.edu>
2021-09-28 20:25:36 -0700

    Implement variables and display function

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello, World!");
+/**********************************************************
+displayTotal()
+Pre: @param: d_t, a_t, t_exp, t_a_exp, t_reim, t_amo
+Post: Displays the total expenses, allowable expenses,
+reimbursement, and the amount saved. No returns
+*/
 
+void displayTotal(t_days,d_t, a_t, t_exp, t_a_exp, t_reim, t_amo){
+    printf("We made it");
+}
+int main(){
+    /// Declaration assignments
+
+    int total_days;
+
+    double depart_time, arrival_time,
+        total_expenses = 0, total_allowable_expenses = 0,
+        total_reimbursement = 0, total_amount_saved = 0;
+
+    displayTotal(total_days, depart_time, arrival_time, total_expenses,
+                 total_allowable_expenses, total_reimbursement, total_amount_saved);
     return 0;
 }
